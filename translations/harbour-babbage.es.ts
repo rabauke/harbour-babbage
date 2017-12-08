785adc7f547c51ada33d7d1e92ea9b1b8f627851
Carmen F. B <6286776+carmenfdezb@users.noreply.github.com>
2017-12-08 02:02:20 +0100

    Added Spanish language

---
+++ translations/harbour-babbage.es.ts
@@ -0,0 +1,319 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es">
+<context>
+    <name>About</name>
+    <message>
+        <source>About Babbage</source>
+        <translation>Acerca de Babbage</translation>
+    </message>
+    <message>
+        <source>_discription_</source>
+        <translation>Babbage es una calculadora de bolsillo simple con opción de calculadora científica.  La calculadora científica analiza las expresiones matemáticas que usan una notación estándar.  Los operadores matemáticos de suma (+), resta (-), multiplicación (*), división (/), exponenciación (^), factorial (!) y grados de ángulo (º) son compatibles.  Es posible usar distintas funciones matemáticas.  Los resultados se pueden asignar a variables.  Los nombres de variables se componen de símbolos alfanuméricos y empiezan con letra. Se usa pi para π y e para el número de Euler. También es posible usar deg precedido de un espacio en lugar del operador de grado º.</translation>
+    </message>
+    <message>
+        <source>List of functions</source>
+        <translation>Lista de funciones</translation>
+    </message>
+</context>
+<context>
+    <name>Functions</name>
+    <message>
+        <source>List of functions</source>
+        <translation>Lista de funciones</translation>
+    </message>
+    <message>
+        <source>Basic functions</source>
+        <translation>Funciones básicas</translation>
+    </message>
+    <message>
+        <source>abs_description</source>
+        <translation>Valor absoluto de x.</translation>
+    </message>
+    <message>
+        <source>mod_description</source>
+        <translation>Coma flotante que resulta de la división de x/y.</translation>
+    </message>
+    <message>
+        <source>sqrt_description</source>
+        <translation>Raíz cuadrada de x.</translation>
+    </message>
+    <message>
+        <source>Rounding functions</source>
+        <translation>Funciones de redondeo</translation>
+    </message>
+    <message>
+        <source>round_description</source>
+        <translation>Valor de x redondeado al entero más próximo.</translation>
+    </message>
+    <message>
+        <source>ceil_description</source>
+        <translation>Valor de x redondeado al entero mayor más próximo.</translation>
+    </message>
+    <message>
+        <source>floor_description</source>
+        <translation>Valor de x redondeado al entero menor más próximo.</translation>
+    </message>
+    <message>
+        <source>Trigonometric functions</source>
+        <translation>Funciones trigonométricas</translation>
+    </message>
+    <message>
+        <source>sin_description</source>
+        <translation>Seno de x.</translation>
+    </message>
+    <message>
+        <source>cos_description</source>
+        <translation>Coseno de x.</translation>
+    </message>
+    <message>
+        <source>tan_description</source>
+        <translation>Tangente de x.</translation>
+    </message>
+    <message>
+        <source>cot_description</source>
+        <translation>Cotangente de x.</translation>
+    </message>
+    <message>
+        <source>Inverse trigonometric functions</source>
+        <translation>Funciones trigonométricas inversas</translation>
+    </message>
+    <message>
+        <source>asin_description</source>
+        <translation>Arcoseno de x.</translation>
+    </message>
+    <message>
+        <source>acos_description</source>
+        <translation>Arcocoseno de x.</translation>
+    </message>
+    <message>
+        <source>atan_description</source>
+        <translation>Arcotangente de x o arcotangente de y/x respetando el signo de x e y, respectivamente.</translation>
+    </message>
+    <message>
+        <source>acot_description</source>
+        <translation>Arcocotangente de x.</translation>
+    </message>
+    <message>
+        <source>sinh_description</source>
+        <translation>Seno hiperbólico de x.</translation>
+    </message>
+    <message>
+        <source>cosh_description</source>
+        <translation>Coseno hiperbólico de x.</translation>
+    </message>
+    <message>
+        <source>tanh_description</source>
+        <translation>Tangente hiperbólica de x.</translation>
+    </message>
+    <message>
+        <source>coth_description</source>
+        <translation>Cotangente hiperbólica de x.</translation>
+    </message>
+    <message>
+        <source>asinh_description</source>
+        <translation>Arcoseno hiperbólico de x.</translation>
+    </message>
+    <message>
+        <source>acosh_description</source>
+        <translation>Arcocoseno hiperbólico de x.</translation>
+    </message>
+    <message>
+        <source>atanh_description</source>
+        <translation>Arcotangente hiperbólica de x.</translation>
+    </message>
+    <message>
+        <source>acoth_description</source>
+        <translation>Arcocotangente hiperbólica de x.</translation>
+    </message>
+    <message>
+        <source>Exponential and related functions</source>
+        <translation>Función exponencial y relacionadas</translation>
+    </message>
+    <message>
+        <source>exp_description</source>
+        <translation>Exponencial de x.</translation>
+    </message>
+    <message>
+        <source>ln_description</source>
+        <translation>Logaritmo neperiano de x.</translation>
+    </message>
+    <message>
+        <source>log_description</source>
+        <translation>Logaritmo de x en base 10 o en base b, respectivamente.</translation>
+    </message>
+    <message>
+        <source>Error functions and related functions</source>
+        <translation>	Función de error y relacionadas</translation>
+    </message>
+    <message>
+        <source>erf_description</source>
+        <translation>Función error.</translation>
+    </message>
+    <message>
+        <source>erfc_description</source>
+        <translation>Función error complementaria.</translation>
+    </message>
+    <message>
+        <source>normal_description</source>
+        <translation>Función de distribución Normal (0,1).</translation>
+    </message>
+    <message>
+        <source>invnormal_description</source>
+        <translation>La inversa de una función de distribución Normal (0,1).</translation>
+    </message>
+    <message>
+        <source>Studentt_description</source>
+        <translation>Función de distribución t-Student con n grados de libertad.</translation>
+    </message>
+    <message>
+        <source>invStudentt_description</source>
+        <translation>La inversa de la función de distribución t-Student con n grados de libertad.</translation>
+    </message>
+    <message>
+        <source>Gamma function and related functions</source>
+        <translation>Función gamma y relacionadas</translation>
+    </message>
+    <message>
+        <source>Gamma_description</source>
+        <translation>Función gamma y función gamma incompleta superior.</translation>
+    </message>
+    <message>
+        <source>Beta_description</source>
+        <translation>Función beta.</translation>
+    </message>
+    <message>
+        <source>binomial_description</source>
+        <translation>Coeficiente binomial.</translation>
+    </message>
+    <message>
+        <source>Statistical functions</source>
+        <translation>Funciones estadísticas</translation>
+    </message>
+    <message>
+        <source>min_description</source>
+        <translation>Valor mínimo de los argumentos dados.</translation>
+    </message>
+    <message>
+        <source>max_description</source>
+        <translation>Valor máximo de los argumentos dados.</translation>
+    </message>
+    <message>
+        <source>mean_description</source>
+        <translation>Media de los argumentos dados.</translation>
+    </message>
+    <message>
+        <source>var_description</source>
+        <translation>Varianza de los argumentos dados.</translation>
+    </message>
+    <message>
+        <source>std_description</source>
+        <translation>Desviación típica de los argumentos dados.</translation>
+    </message>
+    <message>
+        <source>Hyperbolic functions</source>
+        <translation>Funciones hiperbólicas</translation>
+    </message>
+    <message>
+        <source>Inverse hyperbolic functions</source>
+        <translation>Funciones hiperbólicas inversas</translation>
+    </message>
+    <message>
+        <source>gamma_description</source>
+        <translation>Función gamma incompleta inferior.</translation>
+    </message>
+    <message>
+        <source>median_description</source>
+        <translation>Mediana de los argumentos dados.</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>About Babbage</source>
+        <translation>Acerca de Babbage</translation>
+    </message>
+    <message>
+        <source>Scientific calculator</source>
+        <translation>Calculadora científica</translation>
+    </message>
+    <message>
+        <source>Mathematical expression</source>
+        <translation>Expresión matemática</translation>
+    </message>
+    <message>
+        <source>Copy result</source>
+        <translation>Copiar resultado</translation>
+    </message>
+    <message>
+        <source>Copy formula</source>
+        <translation>Copiar fórmula</translation>
+    </message>
+    <message>
+        <source>Remove all output</source>
+        <translation>Borrar todas las salidas</translation>
+    </message>
+    <message>
+        <source>Removing all output</source>
+        <translation>Borrando todas las salidas</translation>
+    </message>
+    <message>
+        <source>Remove output</source>
+        <translation>Borrar salida</translation>
+    </message>
+    <message>
+        <source>Pocket calculator</source>
+        <translation>Calculadora de bolsillo</translation>
+    </message>
+</context>
+<context>
+    <name>SimpleCalculator</name>
+    <message>
+        <source>Pocket calculator</source>
+        <translation>Calculadora de bolsillo</translation>
+    </message>
+    <message>
+        <source>Get memory</source>
+        <translation>Usar memoria</translation>
+    </message>
+    <message>
+        <source>Save result</source>
+        <translation>Guardar resultado</translation>
+    </message>
+    <message>
+        <source>About Babbage</source>
+        <translation>Acerca de Babbage</translation>
+    </message>
+    <message>
+        <source>Scientific calculator</source>
+        <translation>Calculadora científica</translation>
+    </message>
+</context>
+<context>
+    <name>Variables</name>
+    <message>
+        <source>Variables</source>
+        <translation>Variables</translation>
+    </message>
+    <message>
+        <source>Copy variable</source>
+        <translation>Copiar variable</translation>
+    </message>
+    <message>
+        <source>Copy value</source>
+        <translation>Copiar valor</translation>
+    </message>
+    <message>
+        <source>Clear all variables</source>
+        <translation>Borrar todas las variables</translation>
+    </message>
+    <message>
+        <source>Clearing all variables</source>
+        <translation>Borrando todas las variables</translation>
+    </message>
+    <message>
+        <source>Clear variable</source>
+        <translation>Borrar variable</translation>
+    </message>
+</context>
+</TS>
